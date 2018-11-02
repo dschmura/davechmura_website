2f54687388286ac2e83c27038b05eae0f7527edf
David Chmura <dschmura@humbledaisy.com>
2018-11-02 18:15:51 -0400

    ADD: Migrate images and styles (mostly) from previous version

--- app/javascript/davechmura_website/index.js
+++ app/javascript/davechmura_website/index.js
@@ -3,6 +3,7 @@
 
   // Include internal resources for this app
   import './javascripts/application'
+  import './javascripts/agency.min'
 
   require.context("./stylesheets", true, /\.(css|scss|sass)$/)
   require.context('./images/', true, /.(gif|jpg|jpeg|png|svg)$/)
