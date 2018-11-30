c488cfb68a93ff244b2b2b402c4a4e34612d213d
David Chmura <dschmura@humbledaisy.com>
2018-11-30 11:45:40 -0500

    UPDATE: Webpack config to fix postcss-loader issue.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,3 +8,12 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
 }));
 
 module.exports = environment
+
+
+// For production deployments?
+const path = require("path")
+Object.assign(environment.loaders.get("css").use.find(el => el.loader === "postcss-loader").options, {
+  config: {
+    path: path.resolve(__dirname, "../..", ".postcssrc.yml")
+  }
+})
