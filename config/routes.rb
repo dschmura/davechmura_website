7a8925fc8d585ca1cdc767b9c267aaa2fdf79f11
David Chmura <dschmura@humbledaisy.com>
2019-01-03 07:55:20 -0500

    UPDATE: Continuing to refine styles and remove bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get '/twitter' => redirect('https://twitter.com/humbledaisy')
   get '/github' => redirect('https://github.com/dschmura')
   get '/linkedin' => redirect('https://www.linkedin.com/in/davechmura/')
+  get '/umich' => redirect('https://www.umich.edu')
 
   match '/resume', :to => 'resume#index', via: :get
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
