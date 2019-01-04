dbf81f2beaea1ef1155a61332e951aecccc97476
David Chmura <dschmura@humbledaisy.com>
2019-01-04 11:38:34 -0500

    UPDATE: FULLY REMOVED BOOTSTRAP, JQUERY AND POPPER!!!!!
    I am excited, in case you couldn't tell.

--- app/javascript/davechmura_website/index.js
+++ app/javascript/davechmura_website/index.js
@@ -1,13 +1,13 @@
   // Include external resources for this app_files
-  import 'bootstrap'
+  // import 'bootstrap'
   import 'actiontext'
   import 'trix/dist/trix.css'
   // import fontawesome from "@fortawesome/fontawesome-free";
   import '@fortawesome/fontawesome-free/js/all';
-  
+
   // Include internal resources for this app
   import './javascripts/application'
-  import './javascripts/agency.min'
+  // import './javascripts/agency.min'
 
   require.context("./stylesheets", true, /\.(css|scss|sass)$/)
   require.context('./images/', true, /.(gif|jpg|jpeg|png|svg)$/)
